95fcdad101cc2678551b8cdf7e07a26c43ad97bf
nzeng8 <taleorca@gmail.com>
2024-06-06 17:57:36 -0700

    refactor: swapped listeners for event enums

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import * as dotenv from "dotenv";
 import { onMessageCreate } from "./events/messageCreate";
 import { onReady } from "./events/ready";
@@ -9,7 +9,7 @@ const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-client.on("ready", () => onReady(client));
-client.on("messageCreate", (message) => onMessageCreate(client, message));
+client.on(Events.ClientReady, () => onReady(client));
+client.on(Events.MessageCreate, (message) => onMessageCreate(client, message));
 
 client.login(process.env.TOKEN);
