f0d962165aba48716c98d2f49a566975db317a20
nzeng8 <taleorca@gmail.com>
2024-06-06 18:31:31 -0700

    feat: changed to OOP structure

--- src/bot.ts
+++ src/bot.ts
@@ -3,11 +3,42 @@ import { onMessageCreate } from "./events/messageCreate";
 import { onReady } from "./events/ready";
 import { config } from "./config";
 
-const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
-});
+class Bot {
 
-client.on(Events.ClientReady, () => onReady(client));
-client.on(Events.MessageCreate, (message) => onMessageCreate(client, message));
+	private client: Client;
 
-client.login(config.DISCORD_TOKEN);
+	constructor() {
+		this.client = new Client({
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMessages,
+				GatewayIntentBits.MessageContent,
+			],
+			shards: "auto",
+			failIfNotExists: false,
+		});
+	}
+
+	init() {
+		this.client
+			.login(config.DISCORD_TOKEN)
+			.then(() => {
+				this.addClientEventHandlers();
+			})
+			.catch((err) => {
+				console.error("Error starting bot", err);
+			});
+	}
+
+	addClientEventHandlers() {
+		this.client.on(Events.ClientReady, () => onReady(this.client));
+		this.client.on(Events.Error, (err: Error) => {
+			console.error("Client error", err);
+		});
+
+		this.client.on(Events.MessageCreate, message => onMessageCreate(this.client, message));
+	}
+}
+
+const app = new Bot();
+app.init();
