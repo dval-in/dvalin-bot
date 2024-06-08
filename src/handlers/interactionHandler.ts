f2da5e14cac365d435bac8c320b9b48ad035a184
nzeng8 <taleorca@gmail.com>
2024-06-07 19:03:41 -0700

    refactor: added possible button and select menu handling

--- src/handlers/interactionHandler.ts
+++ src/handlers/interactionHandler.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction } from "discord.js";
+import { Interaction, ChatInputCommandInteraction, ButtonInteraction } from "discord.js";
 import { Command } from "../commands/command";
 import { PingCommand } from "../commands/misc/ping";
 
@@ -6,29 +6,37 @@ export class InteractionHandler {
 	private commands: Command[];
 
 	constructor() {
-		this.commands = [new PingCommand()];
+		this.commands = [
+			new PingCommand(),
+		];
 	}
 
 	getSlashCommands() {
 		return this.commands.map((command: Command) => command.slashCommandConfig.toJSON());
 	}
 
-	async handleInteraction(interaction: ChatInputCommandInteraction): Promise<void> {
-		const commandName = interaction.commandName;
-
-		const matchedCommand = this.commands.find((command) => command.name === commandName);
-
-		if (!matchedCommand) {
-			return Promise.reject("Command not matched.");
+	async handleInteraction(interaction: Interaction): Promise<void> {
+		if (interaction.isCommand()) {
+			const commandName = interaction.commandName;
+	
+			const matchedCommand = this.commands.find((command) => command.name === commandName);
+	
+			if (!matchedCommand) {
+				return Promise.reject("Command not matched.");
+			}
+	
+			matchedCommand
+				.execute(<ChatInputCommandInteraction> interaction)
+				.then(() => {
+					console.log(`Successfully executed command [/${interaction.commandName}]`);
+				})
+				.catch((err) => {
+					console.log(`Error executing command [/${interaction.commandName}]: ${err}`);
+				});
+		} else if (interaction.isButton()) {
+			// Responding to global buttons.
+		} else if (interaction.isStringSelectMenu()) {
+			// Responding to global select menus. 
 		}
-
-		matchedCommand
-			.execute(interaction)
-			.then(() => {
-				console.log(`Successfully executed command [/${interaction.commandName}]`);
-			})
-			.catch((err) => {
-				console.log(`Error executing command [/${interaction.commandName}]: ${err}`);
-			});
 	}
 }
