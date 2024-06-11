b13b4ad468d2c3318b5f8f73bb327e0a30b23470
Legendaxicad <dingxy2772@gmail.com>
2024-06-11 22:06:52 +0700

    feat: Added pityCalc command

---
+++ src/commands/misc/pityCalc.ts
@@ -0,0 +1,13 @@
+import { 
+    SlashCommandBuilder, 
+    EmbedBuilder, 
+    ButtonBuilder, 
+    ButtonStyle, 
+    ButtonInteraction,
+    ActionRowBuilder, 
+    PermissionFlagsBits, 
+    ComponentType, 
+} from "discord.js";
+import { Command } from "../../types/command";
+import { config } from "../../config";
+
